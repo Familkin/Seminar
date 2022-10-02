89abc3a13eec1a67cfc408ee50f199589ebb754d
Familkin <cepjil@mail.ru>
2022-10-02 19:18:18 +0300

    Решили первую задачу

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Привет");
+﻿// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Квадрат {number} равен {number*number}");
+
+string name = Console.ReadLine();
+Console.WriteLine("Ваше имя: "+name);
