30479986efb466c6e97ec7ffb960ce11868cb54b
Familkin <cepjil@mail.ru>
2022-10-04 22:16:09 +0300

    Семинар 2

---
+++ Chernovik/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 2 Напишите программу, которая выводит случайное число из отрезка [10, 9999] и показывает наибольшую цифру числа. 
+
+// Через массив
+
+
+int N = int.Parse(Console.ReadLine());
+int[] array = new int[N]; //Обозначаю массив
+
+            for (int j = 0; j < i.Length-1; j++)
+                textBox1.AppendText(i[j].ToString());
